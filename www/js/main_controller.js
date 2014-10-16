e74dad9e6320f94b862c359632aefded4213a722
Liza Carter <lizahcarter@gmail.com>
2014-10-16 10:33:19 -0500

    initial commit

---
+++ www/js/main_controller.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+  angular.module('lls-golf')
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+    $scope.greeting = 'hello';
+  }]);
+})();
