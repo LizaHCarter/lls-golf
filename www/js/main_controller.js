d35136f2af43364db040cebed675d40f48006976
Liza Carter <lizahcarter@gmail.com>
2014-10-16 11:59:08 -0500

    added gyroscope plugin

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -3,5 +3,20 @@
   angular.module('lls-golf')
   .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
     $scope.greeting = 'hello';
+    function success(orientation){
+      console.log(orientation.alpha);
+      console.log(orientation.beta);
+      console.log(orientation.gamma);
+      $scope.orientation = orientation;
+      $scope.$digest();
+    }
+    function error(err){
+      console.log('error', err);
+    }
+
+    $scope.start = function(){
+      console.log('starting gyroscope');
+      navigator.gyroscope.watchGyroscope(success, error, {frequency:100});
+    };
   }]);
 })();
