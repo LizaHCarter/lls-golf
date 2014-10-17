c50a48b6bb82875e9ad2aa50016da036622bdd78
Liza Carter <lizahcarter@gmail.com>
2014-10-17 11:39:37 -0500

    added canvas tag

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -2,15 +2,33 @@
   'use strict';
   angular.module('lls-golf')
   .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
-    $scope.x = document.documentElement.clientWidth;
+    /*$scope.x = document.documentElement.clientWidth;
     $scope.y = document.documentElement.clientHeight - 40;
     $scope.ballX = ((5 * $scope.x)/6);
-    $scope.ballY = ((7 * $scope.y)/8);
+    $scope.ballY = ((7 * $scope.y)/8);*/
+    $scope.canvasHeight = window.innerHeight;
+    $scope.canvasWidth = window.innerWidth;
+
+    function init(){
+      var can = document.getElementById('golfCanvas'),
+          ctx = can.getContext('2d');
+
+      function drawHole(){
+        ctx.fillStyle= 'black';
+        ctx.fillRect(20,20, 50, 50);
+        /*ctx.beginPath();
+        ctx.arc(10, 10, 5, 0, Math.PI*2, false);
+        ctx.closePath();
+        ctx.fill();*/
+      }
+      drawHole();
+    }
 
     window.addEventListener('deviceorientation', function(data){
       $scope.data = data;
       console.log(data);
       $scope.$digest();
     });
+    init();
   }]);
 })();
